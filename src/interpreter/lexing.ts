7fccb77362143dae756619f5cf119ec35968d7a6
Zibing Zhang <zibing.zha@gmail.com>
2022-01-01 20:58:35 -0500

    Enhance lexing

--- src/interpreter/lexing.ts
+++ src/interpreter/lexing.ts
@@ -13,15 +13,34 @@ let FALSE_LITERAL_RE = /^#(f|false)$/;
 let INTEGER_RE = /^[+-]?\d+\.?$/;
 let RATIONAL_RE = /^[+-]?\d+\/\d+$/;
 let DECIMAL_RE = /^[+-]?\d*\.\d+$/;
-let DIV_BY_ZERO_RE = /^[+-]?\d+\/0+/;
+let DIV_BY_ZERO_RE = /^[+-]?\d+\/0+$/;
 
-let BAD_SYNTAX = (syntax: string) => `read-syntax: bad syntax \`${syntax}\``;
-let DIV_BY_ZERO = (number: string) => `read-syntax: division by zero in \`${number}\``;
-let EXPECT_COMMENTED_OUT_ELEMENT = "read-syntax: expected a commented-out element for `#;`, but found end-of-file";
-let EXPECT_ELEMENT_FOR_QUOTING = (found: string) => `read-syntax: expected an element for quoting \"'\", but found ${found}`;
-let ILLEGAL_USE_OF_PERIOD = "read-syntax: illegal use of `.`";
+let BAD_SYNTAX_ERR = (syntax: string) => `read-syntax: bad syntax \`${syntax}\``;
+let DIV_BY_ZERO_ERR = (number: string) => `read-syntax: division by zero in \`${number}\``;
+let EXPECT_CLOSING_PAREN_ERR = (opening: string) => {
+  if (opening === "(") {
+    return "read-syntax: expected a `)` to close preceding `(`";
+  } else if (opening === "[") {
+    return "read-syntax: expected a `]` to close preceding `[`";
+  } else {
+    return "read-syntax: expected a `}` to close preceding `{`";
+  }
+}
+let EXPECT_COMMENTED_OUT_ELEMENT_ERR = "read-syntax: expected a commented-out element for `#;`, but found end-of-file";
+let EXPECT_CORRECT_CLOSING_PAREN_ERR = (opening: string | undefined, found: string): string => {
+  if (opening === "(") {
+    return `read-syntax: expected \`)\` to close preceding \`(\`, found instead \`${found}\``;
+  } else if (opening === "[") {
+    return `read-syntax: expected \`]\` to close preceding \`[\`, found instead \`${found}\``;
+  } else {
+    return `read-syntax: expected \`}\` to close preceding \`{\`, found instead \`${found}\``;
+  }
+}
+let EXPECT_ELEMENT_FOR_QUOTING_ERR = (found: string) => `read-syntax: expected an element for quoting \"'\", but found ${found}`;
+let ILLEGAL_USE_OF_DOT_ERR = "read-syntax: illegal use of `.`";
 let QUASI_QUOTE_UNSUPPORTED_ERR = "read-syntax: quasiquotes not supported";
 let UNCLOSED_STRING_ERR = "read-syntax: expected a closing `\"`";
+let UNEXPECTED_ERR = (found: string) => `read-syntax: unexpected \`${found}\``;
 
 enum State {
   INIT,
@@ -46,6 +65,7 @@ class Lexer implements Stage {
     this.isAtEnd = this.input.length === 0;
 
     let tokens: Token[] = [];
+    let parenStack: Token[] = []; let opening;
     let state = State.INIT;
     let text = "";
     let lineno = 0;
@@ -54,19 +74,23 @@ class Lexer implements Stage {
     let blockCommentDepth = 0;
     let expectingElementToQuote = false;
 
-    let addToken = (lineno: number, colno: number, type: TokenType, text: string) => {
-      tokens.push(new Token(lineno, colno, type, text));
+    let addToken = (lineno: number, colno: number, type: TokenType, text: string): Token => {
+      let token = new Token(lineno, colno, type, text)
+      tokens.push(token);
       text = "";
       expectingElementToQuote = false;
+      return token
     };
     let addNameToken = (lineno: number, colno: number, text: string): StageOutput | undefined => {
-      if (text === "...") {
+      if (text === ".") {
+        return this.error(lineno, colno, text, ILLEGAL_USE_OF_DOT_ERR);
+      } else if (text === "...") {
         addToken(lineno, colno, TokenType.PLACEHOLDER, text);
       } else if (text.match(INTEGER_RE)) {
         addToken(lineno, colno, TokenType.INTEGER, text);
       } else if (text.match(RATIONAL_RE)) {
         if (text.match(DIV_BY_ZERO_RE)) {
-          return this.error(lineno, colno, text, DIV_BY_ZERO(text));
+          return this.error(lineno, colno, text, DIV_BY_ZERO_ERR(text));
         } else {
           addToken(lineno, colno, TokenType.RATIONAL, text);
         }
@@ -82,27 +106,37 @@ class Lexer implements Stage {
 
       switch (state) {
         case State.INIT: {
+          text = ch;
           if (ch.match(QUASI_QUOTE_RE)) {
             return this.error(lineno, colno, ch, QUASI_QUOTE_UNSUPPORTED_ERR);
           } else if (ch.match(/\s/)) {
             // skip
           } else if (ch.match(LEFT_PAREN_RE)) {
-            addToken(lineno, colno, TokenType.LEFT_PAREN, ch);
+            parenStack.push(addToken(lineno, colno, TokenType.LEFT_PAREN, ch));
           } else if (ch.match(RIGHT_PAREN_RE)) {
+            if (parenStack.length === 0) { return this.error(lineno, colno, ch, UNEXPECTED_ERR(ch)); }
+            if (!this.matches(opening = parenStack.pop()?.text, ch)) { return this.error(lineno, colno, ch, EXPECT_CORRECT_CLOSING_PAREN_ERR(opening, ch)); }
             addToken(lineno, colno, TokenType.RIGHT_PAREN, ch);
           } else if (ch === "\"") {
-            text = ch;
             state = State.STRING;
           } else if (ch === "#") {
-            text = ch;
-            state = State.POUND;
+            if (this.peek() === ";") {
+              // turn into flag to ignore next sexpr
+              addToken(lineno, colno, TokenType.SEXPR_COMMENT, text + ch);
+            } else if (this.peek() === "|") {
+              blockCommentDepth = 0;
+              state = State.BLOCK_COMMENT;
+            } else if (this.peek(2).match(/!\/?/)) {
+              state = State.LINE_COMMENT;
+            } else {
+              state = State.POUND;
+            }
           } else if (ch === "'") {
             addToken(lineno, colno, TokenType.QUOTE, ch);
             state = State.QUOTE;
           } else if (ch === ";") {
             state = State.LINE_COMMENT;
           } else {
-            text = ch;
             state = State.NAME;
           }
           break;
@@ -160,30 +194,27 @@ class Lexer implements Stage {
           if (ch.match(QUASI_QUOTE_RE)) {
             return this.error(lineno, colno, ch, QUASI_QUOTE_UNSUPPORTED_ERR);
           } else if (ch.match(/\s/)) {
-            if (text === ".") { return this.error(lineno, colno, text, ILLEGAL_USE_OF_PERIOD); }
             let error = addNameToken(lineno, colno, text);
             if (error) { return error; }
             state = State.INIT;
           } else if (ch.match(LEFT_PAREN_RE)) {
-            if (text === ".") { return this.error(lineno, colno, text, ILLEGAL_USE_OF_PERIOD); }
             let error = addNameToken(lineno, colno, text);
             if (error) { return error; }
-            addToken(lineno, colno + 1, TokenType.LEFT_PAREN, ch);
+            parenStack.push(addToken(lineno, colno + 1, TokenType.LEFT_PAREN, ch));
             state = State.INIT;
           } else if (ch.match(RIGHT_PAREN_RE)) {
-            if (text === ".") { return this.error(lineno, colno, text, ILLEGAL_USE_OF_PERIOD); }
             let error = addNameToken(lineno, colno, text);
             if (error) { return error; }
+            if (parenStack.length === 0) { return this.error(lineno, colno, ch, UNEXPECTED_ERR(ch)); }
+            if (!this.matches(opening = parenStack.pop()?.text, ch)) { return this.error(lineno, colno, ch, EXPECT_CORRECT_CLOSING_PAREN_ERR(opening, ch)); }
             addToken(lineno, colno + 1, TokenType.RIGHT_PAREN, ch);
             state = State.INIT;
           } else if (ch === "\"") {
-            if (text === ".") { return this.error(lineno, colno, text, ILLEGAL_USE_OF_PERIOD); }
             let error = addNameToken(lineno, colno, text);
             if (error) { return error; }
             text = ch;
             state = State.STRING;
           } else if (ch === "'") {
-            if (text === ".") { return this.error(lineno, colno, text, ILLEGAL_USE_OF_PERIOD); }
             let error = addNameToken(lineno, colno, text);
             if (error) { return error; }
             addToken(lineno, colno + 1, TokenType.QUOTE, ch);
@@ -195,19 +226,6 @@ class Lexer implements Stage {
         }
 
         case State.POUND: {
-          // if statements can probably be avoided by peeking
-          if (text === "#" && ch === ";") {
-            // turn into flag to ignore next sexpr
-            addToken(lineno, colno, TokenType.SEXPR_COMMENT, text + ch);
-            state = State.INIT;
-            continue;
-          }
-          if (text === "#" && ch === '|') {
-            blockCommentDepth = 0;
-            state = State.BLOCK_COMMENT;
-            continue;
-          }
-
           if (ch.match(QUASI_QUOTE_RE)) {
             return this.error(lineno, colno, ch, QUASI_QUOTE_UNSUPPORTED_ERR);
           } else if (ch.match(/\s/)) {
@@ -216,7 +234,7 @@ class Lexer implements Stage {
             } else if (text.match(FALSE_LITERAL_RE)) {
               addToken(lineno, colno, TokenType.FALSE, text);
             } else {
-              return this.error(lineno, colno, text, BAD_SYNTAX(text));
+              return this.error(lineno, colno, text, BAD_SYNTAX_ERR(text));
             }
             state = State.INIT;
           } else if (ch.match(LEFT_PAREN_RE)) {
@@ -225,9 +243,9 @@ class Lexer implements Stage {
             } else if (text.match(FALSE_LITERAL_RE)) {
               addToken(lineno, colno, TokenType.FALSE, text);
             } else {
-              return this.error(lineno, colno, text, BAD_SYNTAX(text));
+              return this.error(lineno, colno, text, BAD_SYNTAX_ERR(text));
             }
-            addToken(lineno, colno + 1, TokenType.LEFT_PAREN, ch);
+            parenStack.push(addToken(lineno, colno + 1, TokenType.LEFT_PAREN, ch));
             state = State.INIT;
           } else if (ch.match(RIGHT_PAREN_RE)) {
             if (text.match(TRUE_LITERAL_RE)) {
@@ -235,8 +253,10 @@ class Lexer implements Stage {
             } else if (text.match(FALSE_LITERAL_RE)) {
               addToken(lineno, colno, TokenType.FALSE, text);
             } else {
-              return this.error(lineno, colno, text, BAD_SYNTAX(text));
+              return this.error(lineno, colno, text, BAD_SYNTAX_ERR(text));
             }
+            if (parenStack.length === 0) { return this.error(lineno, colno, ch, UNEXPECTED_ERR(ch)); }
+            if (!this.matches(opening = parenStack.pop()?.text, ch)) { return this.error(lineno, colno, ch, EXPECT_CORRECT_CLOSING_PAREN_ERR(opening, ch)); }
             addToken(lineno, colno + 1, TokenType.RIGHT_PAREN, ch);
             state = State.INIT;
           } else if (ch === "\"") {
@@ -245,7 +265,7 @@ class Lexer implements Stage {
             } else if (text.match(FALSE_LITERAL_RE)) {
               addToken(lineno, colno, TokenType.FALSE, text);
             } else {
-              return this.error(lineno, colno, text, BAD_SYNTAX(text));
+              return this.error(lineno, colno, text, BAD_SYNTAX_ERR(text));
             }
             text = ch;
             state = State.STRING;
@@ -255,7 +275,7 @@ class Lexer implements Stage {
             } else if (text.match(FALSE_LITERAL_RE)) {
               addToken(lineno, colno, TokenType.FALSE, text);
             } else {
-              return this.error(lineno, colno, text, BAD_SYNTAX(text));
+              return this.error(lineno, colno, text, BAD_SYNTAX_ERR(text));
             }
             addToken(lineno, colno + 1, TokenType.QUOTE, ch);
             state = State.QUOTE;
@@ -271,10 +291,10 @@ class Lexer implements Stage {
           } else if (ch.match(/\s/)) {
             // skip
           } else if (ch.match(LEFT_PAREN_RE)) {
-            addToken(lineno, colno, TokenType.LEFT_PAREN, ch);
+            parenStack.push(addToken(lineno, colno, TokenType.LEFT_PAREN, ch));
             state = State.INIT;
           } else if (ch.match(RIGHT_PAREN_RE)) {
-            return this.error(lineno, colno, ch, EXPECT_ELEMENT_FOR_QUOTING(ch));
+            return this.error(lineno, colno, ch, EXPECT_ELEMENT_FOR_QUOTING_ERR(ch));
           } else if (ch === "\"") {
             text = ch;
             state = State.STRING;
@@ -311,7 +331,8 @@ class Lexer implements Stage {
 
     switch (state) {
       case State.NAME: {
-        addToken(lineno, colno, TokenType.NAME, text);
+        let error = addNameToken(lineno, colno, text);
+        if (error) { return error; }
         break;
       }
 
@@ -321,15 +342,15 @@ class Lexer implements Stage {
         } else if (text.match(FALSE_LITERAL_RE)) {
           addToken(lineno, colno, TokenType.FALSE, text);
         } else if (text.match(/#;(.*;.*)?/)) {
-          return this.error(lineno, colno, text, EXPECT_COMMENTED_OUT_ELEMENT);
+          return this.error(lineno, colno, text, EXPECT_COMMENTED_OUT_ELEMENT_ERR);
         } else {
-          return this.error(lineno, colno, text, BAD_SYNTAX(text));
+          return this.error(lineno, colno, text, BAD_SYNTAX_ERR(text));
         }
         break;
       }
 
       case State.QUOTE: {
-        return this.error(lineno, colno, text, EXPECT_ELEMENT_FOR_QUOTING('end-of-file'));
+        return this.error(lineno, colno, text, EXPECT_ELEMENT_FOR_QUOTING_ERR('end-of-file'));
       }
 
       case State.STRING: {
@@ -338,7 +359,11 @@ class Lexer implements Stage {
     }
 
     if (expectingElementToQuote) {
-      return this.error(lineno, colno, text, EXPECT_ELEMENT_FOR_QUOTING('end-of-file'));
+      return this.error(lineno, colno, text, EXPECT_ELEMENT_FOR_QUOTING_ERR('end-of-file'));
+    }
+
+    if (opening = parenStack.pop()) {
+      return this.error(lineno, colno, opening.text, EXPECT_CLOSING_PAREN_ERR(opening.text));
     }
 
     for (let token of tokens) {
@@ -346,6 +371,15 @@ class Lexer implements Stage {
     }
 
     return new StageOutput(tokens);
+  }
+
+  matches(left: string | undefined, right: string): boolean {
+    switch (left) {
+      case "(": return right === ")";
+      case "[": return right === "]";
+      case "{": return right === "}";
+      default: return false;
+    }
   }
 
   // could probably define in the above method and do away with lineno and colno params
@@ -357,6 +391,10 @@ class Lexer implements Stage {
     this.position++;
     this.checkAtEnd();
     return this.input[this.position - 1];
+  }
+
+  peek(n: number = 1): string {
+    return this.input.slice(this.position, Math.min(this.position + n, this.input.length));
   }
 
   checkAtEnd() {
